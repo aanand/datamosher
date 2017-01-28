6de715d12ae954de6fe273daaa4ba2a0d395b9fb
Aanand Prasad <aanand.prasad@gmail.com>
2017-01-28 01:13:51 +0000

    Ignore API errors when pruning friends

--- prune_followers.py
+++ prune_followers.py
@@ -1,4 +1,5 @@
 from bot import DataMosher, start_logging
+from tweepy import TweepError
 
 start_logging()
 
@@ -8,6 +9,9 @@ for friend_id in b.state['friends']:
     user = b.api.get_user(friend_id)
     if user.friends_count > b.config['autofollow_max_following']:
         b.log("unfollowing @{} (following {})".format(user.screen_name, user.friends_count))
-        b.api.destroy_friendship(friend_id)
+        try:
+            b.api.destroy_friendship(friend_id)
+        except TweepError as e:
+            b._log_tweepy_error('Unable to unfollow user', e)
     else:
         b.log("@{} is cool (following {})".format(user.screen_name, user.friends_count))
