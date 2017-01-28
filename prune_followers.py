6f0edd31d4de9d1ead3e4213f8be055c77eea13c
Aanand Prasad <aanand.prasad@gmail.com>
2017-01-28 01:17:28 +0000

    Ignore API error when checking friends count

--- prune_followers.py
+++ prune_followers.py
@@ -6,12 +6,12 @@ start_logging()
 b = DataMosher()
 
 for friend_id in b.state['friends']:
-    user = b.api.get_user(friend_id)
-    if user.friends_count > b.config['autofollow_max_following']:
-        b.log("unfollowing @{} (following {})".format(user.screen_name, user.friends_count))
-        try:
+    try:
+        user = b.api.get_user(friend_id)
+        if user.friends_count > b.config['autofollow_max_following']:
+            b.log("unfollowing @{} (following {})".format(user.screen_name, user.friends_count))
             b.api.destroy_friendship(friend_id)
-        except TweepError as e:
-            b._log_tweepy_error('Unable to unfollow user', e)
-    else:
-        b.log("@{} is cool (following {})".format(user.screen_name, user.friends_count))
+        else:
+            b.log("@{} is cool (following {})".format(user.screen_name, user.friends_count))
+    except TweepError as e:
+        b._log_tweepy_error('Unable to check user', e)
