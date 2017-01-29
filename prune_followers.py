5fd1fc508cad0c11c3d2a46f44ddc6f8090a34fc
Aanand Prasad <aanand.prasad@gmail.com>
2017-01-29 01:01:20 +0000

    Do batch lookup of users in prune script

--- prune_followers.py
+++ prune_followers.py
@@ -1,17 +1,31 @@
 from bot import DataMosher, start_logging
 from tweepy import TweepError
+from time import sleep
+
+# Max user ids per lookup request
+BATCH_SIZE = 100
+
+# Time to wait between lookup requests
+LOOKUP_DELAY = (15 * 60 / 300) + 1
+
+# Time to wait between unfollow requests
+UNFOLLOW_DELAY = 1
 
 start_logging()
 
 b = DataMosher()
+friends = b.state['friends']
 
-for friend_id in b.state['friends']:
-    try:
-        user = b.api.get_user(friend_id)
-        if user.friends_count > b.config['autofollow_max_following']:
-            b.log("unfollowing @{} (following {})".format(user.screen_name, user.friends_count))
-            b.api.destroy_friendship(friend_id)
-        else:
-            b.log("@{} is cool (following {})".format(user.screen_name, user.friends_count))
-    except TweepError as e:
-        b._log_tweepy_error('Unable to check user', e)
+for i in range(0, len(friends), BATCH_SIZE):
+    users = b.api.lookup_users(user_ids=friends[i:i+BATCH_SIZE])
+
+    for user in users:
+        try:
+            if user.friends_count > b.config['autofollow_max_following']:
+                b.log("unfollowing @{} (following {})".format(user.screen_name, user.friends_count))
+                b.api.destroy_friendship(user.id)
+                sleep(UNFOLLOW_DELAY)
+        except TweepError as e:
+            b._log_tweepy_error('Unable to unfollow user', e)
+
+    sleep(LOOKUP_DELAY)
